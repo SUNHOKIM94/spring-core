a470214b1bf4e635678ff14111b30a302d10216b
kimsunho940904 <hkkbc123@gmail.com>
2024-04-01 17:15:46 +0900

    commit

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,15 +5,17 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<>();
+	private static Map<Long, Member> store = new HashMap<>();
 
-    @Override
-    public void save(Member member) {
-        store.put(member.getId(), member);
-    }
+	@Override
+	public void save( Member member ) {
 
-    @Override
-    public Member findById(Long memberId) {
-        return store.get(memberId);
-    }
+		store.put( member.getId(), member );
+	}
+
+	@Override
+	public Member findById( Long memberId ) {
+
+		return store.get( memberId );
+	}
 }
